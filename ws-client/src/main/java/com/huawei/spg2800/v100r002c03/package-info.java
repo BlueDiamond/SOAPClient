ff766e25f96f754c8fbda4a7a6c20f1b516674bf
Pal Ram <rpalanisamy@yahoo.com>
2013-03-30 22:31:49 -0400

    refactored

--- ws-client/src/main/java/com/huawei/spg2800/v100r002c03/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.huawei.com/SPG2800/V100R002C03", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.huawei.spg2800.v100r002c03;
